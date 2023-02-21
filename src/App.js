1e60de311772568ddbec9dfb62f1b0486ec0c03b
EliasFredoni <fredoni.elias@gmail.com>
2023-02-21 08:35:40 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import './App.css'
 import React from 'react'
 
-
 const data = require("./building.json")
+
+
 
 function App() {
 
@@ -13,25 +14,28 @@ const onChange = (event) => {
   setSearch(event.target.value);
 }
 
+const sort = () => {
+  data.sort((a,b) => a.emission > b.emission ? 1 : -1)
+}
+
   return (
     <div className="App">
   <h1>: _Admin_Dahboard_ :</h1>
 
 <div >
-  <button onClick={() => data.sort((a,b) => a.emission < b.emission ? -1 : 1)}>Emission sortieren</button>
+  <button className='sort_button' onClick={sort}>Emission sortieren</button>
 </div>
 
   <input type='text' value={search} onChange={onChange} placeholder='Suche Straßennamen'/>
 
   <div className='data'>
     {data
-    .sort((a,b) => a.emission > b.emission ? 1 : -1)
     .filter(item => item.street.includes(search))
     .map(data => {
       return(
         <div key={data.id}>
-        <input type="checkbox" id="favorite" name="favorite" value="favorite"/>
-        {data.street} - {data.emission} &#11088;
+        <input type="checkbox"/>
+        {data.street} - {data.emission} 
         </div>
       )
     })}
